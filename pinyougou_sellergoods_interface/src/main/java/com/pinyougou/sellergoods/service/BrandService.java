ff568cafbb8b86856a718269c40ecceb87cf9af1
qq429307772 <429307772@qq.com>
2019-05-13 08:06:31 +0800

    init project

---
+++ pinyougou_sellergoods_interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,16 @@
+package com.pinyougou.sellergoods.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+/**
+ * 品牌接口
+ *
+ * @author 周某人
+ */
+public interface BrandService {
+
+    public List<TbBrand> findAll();
+
+}
---
+++ pinyougou_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service          //这里注解使用dubbo的注解
+public class BrandServiceImpl implements BrandService {
+
+    @Autowired
+    private TbBrandMapper brandMapper;
+
+    @Override
+    public List<TbBrand> findAll() {
+        return brandMapper.selectByExample(null);
+    }
+}
